a115bfbc262f61fe9073baa684ea87f8721f22f0
Mathieu Dutour <mathieu@dutour.me>
2020-03-14 21:18:21 +0100

    add comments

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,15 @@
+/**
+ * Returns true for
+ * - /foo
+ * - ./foo
+ * - ../foo
+ * - foo
+ *
+ * and false for
+ * - https://foo
+ * - http://foo
+ * - //foo
+ */
 export function isInternal(url: string): boolean {
   return /^(?:\.{0,2}\/(?!\/)|(?!https?:\/\/)[\w\d])/.test(url);
 }
